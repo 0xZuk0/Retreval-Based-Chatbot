139e15636edb6ac503de658e09dc14e80193c7ab
Dipankar Lama <lamadipankar35@gmail.com>
2019-11-13 12:12:16 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -7,13 +7,13 @@
 
 import numpy as np                                                 # importing modules
 import pandas as pd                                                # numpy for arrays and other functions
-from sklearn.feature_extrction.txt import TfidfVextorizer          # sklearn for vectorizer and codine similarity btw words
+from sklearn.feature_extrction.text import TfidfVectorizer          # sklearn for vectorizer and codine similarity btw words
 from sklearn.metrics.pairwise import cosine_similarity             # pandas for reading csv file
 
 
 # Reading csv file
 
-df = read_csv('aws_faq.csv')
+df = pd.read_csv('aws_faq.csv')
 df.dropna(inplace = True)
 
 # Vectorization **Very Important step** #
@@ -35,7 +35,7 @@ vectorizer.fit(np.concatenate((df.Question, df.Answer)))
       similarities = cosine_similarity(input_question_vector, Question_vectors)
       closest = np.argmax(similarities, axis = 1)
       
-      print('BOT : ' + df.Answer.iloc[closest].value[0])
+      print('BOT : ' + df.Answer.iloc[closest].values[0])
       
 
 ------------------------------------------------------END OF THE PROGRAM--------------------------------------------------------
